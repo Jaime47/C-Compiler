9d272fc492482c00571cc7b62f3bb7167ede12e0
Jaime47 <jaimepons00@gmail.com>
2022-09-17 00:03:31 +0200

    hashing.h defined, rdy for implementation

---
+++ hashing.h
@@ -0,0 +1,48 @@
+#ifndef HASHING_H
+#define HASHING_H
+
+#define ID_LEN 100
+
+enum ArgType
+{
+  VARIABLE,
+  PARAMETRO,
+  FUNCION
+};
+enum DataType
+{
+  BOOLEAN,
+  INT
+};
+enum CardinalityType
+{
+  ESCALAR,
+  VECTOR
+};
+
+typedef struct _Info
+{
+  ArgType arg_type;
+  DataType data_type;
+  CardinalityType cardinality_type;
+  int pos;
+} Info;
+
+typedef struct _Entry
+{
+  Info info;
+  char entry_id[ID_LEN];
+} Entry;
+
+typedef struct _Table
+{
+  Entry **entries;
+  int max_index;
+} Table;
+
+Table *create_table(int length);
+void destroy_table(Table &table);
+bool insert_entry(Table &table, Info info);
+bool delete_entry(Table &table, char *key);
+Entry *search_entry(Table &table, char *key);
+#endif
